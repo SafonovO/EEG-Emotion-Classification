9959fbf1e77421043778531ca7670c04daa0bfc3
Reynolds398 <reynoldladestu@gmail.com>
2024-03-30 04:17:58 -0700

    Updated train_model.py to train using SEED IV data instead
    Modified train_model.py to train using the SEED IV dataset instead. It uses the same model as before. Accuracy is 95.7%

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pickle
+import scipy.io as sio
+import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
@@ -13,6 +15,7 @@ def train_and_save():
     X = []
     y = []
 
+    '''
     # Loading the data for all 16 participants using the provided DE features
     for i in range(16):
         path = 'SEED-V/EEG_DE_features/' + str(i+1) + '_123.npz'
@@ -26,6 +29,37 @@ def train_and_save():
             for k in range(data[j].shape[0]):
                 X.append(data[j][k])
                 y.append(label[j][k])
+    '''
+
+    # Load in SEED IV dataset
+    path = 'SEED_IV/eeg_feature_smooth/'
+
+    # Labels defined in the ReadMe.txt
+    session1_label = [1, 2, 3, 0, 2, 0, 0, 1, 0, 1, 2, 1, 1, 1, 2, 3, 2, 2, 3, 3, 0, 3, 0, 3]
+    session2_label = [2, 1, 3, 0, 0, 2, 0, 2, 3, 3, 2, 3, 2, 0, 1, 1, 2, 1, 0, 3, 0, 1, 3, 1]
+    session3_label = [1, 2, 2, 1, 3, 3, 3, 1, 1, 2, 1, 0, 2, 3, 3, 0, 2, 3, 0, 0, 2, 0, 1, 0]
+
+    # Loop for every session (total of 3)
+    for session in range(3):
+        for filename in os.listdir(path + str(session + 1)):
+            data_mat = sio.loadmat(path + str(session + 1) + '/' + filename)  # Load the file in the directory
+
+            # Go through every de_LDS data
+            for i in range(24):  # 24 trials
+                de_lds_str = "de_LDS"
+                lds_data = data_mat[de_lds_str + str(i + 1)]  # Get the lds data
+                lds_data = np.swapaxes(lds_data, 0, 1)  # Swap the first axes with the second axes
+                lds_data = np.reshape(lds_data, (lds_data.shape[0], 310))  # Convert it from 3d array to 2d array
+                for j in range(lds_data.shape[0]):
+                    X.append(lds_data[j])
+
+                    # Add the correct labels based on the session number
+                    if session == 0:
+                        y.append(session1_label[i])
+                    elif session == 1:
+                        y.append(session2_label[i])
+                    else:
+                        y.append(session3_label[i])
 
     print("Loading data complete!")
     # Split the data to training and testing data
@@ -42,4 +76,3 @@ def train_and_save():
     # Save model
     with open('model.pkl', 'wb') as f:
         pickle.dump(clf, f)
-

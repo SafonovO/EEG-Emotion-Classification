3b5c21a0fcf7d6dcc28ab06cb9c1c9e5d3524b75
Reynolds398 <reynoldladestu@gmail.com>
2024-04-03 22:20:29 -0700

    Generalized feature_extraction.py to handle any EEG raw data with 62 channels
    Modified feature_extraction.py to take in EEG raw data with 62 channels instead of a String.

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -2,13 +2,7 @@ from file_read import *
 from de_psd_calulcation import *
 
 
-def extract_from_raw(eeg_name):
-    eeg_mat = read_mat()
-
-    if eeg_mat is None:
-        print("No mat file is read.")
-        return
-
+def extract_from_raw(eeg_raw):
     params = {
         "stftn": 200,
         "fStart": [1, 4, 8, 14, 31],
@@ -18,11 +12,6 @@ def extract_from_raw(eeg_name):
     }
 
     try:
-        return DE_PSD(eeg_mat[eeg_name], params)
+        return DE_PSD(eeg_raw, params)
     except KeyError as err:
         print("No eeg matrix found. ", err)
-
-
-if __name__ == '__main__':
-    psd, de = extract_from_raw("cz_eeg1")
-    print(de)
